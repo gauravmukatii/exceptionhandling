8fc5bac9f7188f0e019b9370adc84e62f68a7580
gaurav_mukati <gaurav.mukati99@gmail.com>
2023-10-09 18:02:41 +0530

    creating a project to implement exceptions

---
+++ src/test/java/com/project/exceptionhandling/ExceptionhandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.exceptionhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionhandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
